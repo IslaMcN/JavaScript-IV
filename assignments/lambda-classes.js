d0b1e1528f938e4f4543ec15855bc92ba90ed323
IslaMcN <51002520+IslaMcN@users.noreply.github.com>
2019-07-25 13:23:49 -0700

    Started the lambda crew

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,52 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(attributes){
+        this.name =attributes.name,
+        this.location =attributes.name,
+        this.phrase = attributes.phrase
+}
+speak(){
+            console.log(`${this.name} says ${this.phrase}`)
+}//method close
+}//class close
+
+class Instructors extends Person{
+    constructor(knowledge){
+        super(knowledge);
+        this.specialty = knowledge.specialty
+        
+    }
+    speak(){
+        console.log(`Hi everybody! I'm ${this.name} and I'm going to teach you about ${this.specialty}.`)
+    }
+
+}
+
+class Students extends Person{
+    constructor(noSleep){
+        super(noSleep);
+        this.beverage = noSleep.beverage,
+        this.grade = noSleep.grade
+    }
+    redEyes(){
+        console.log(`I am so exhuasted. I need to drink ${this.beverage} to stay awake.`)
+    }
+    graduate(){
+        if (this.grade  >= 70){
+            return "Graduated!";
+        }
+        else {
+            return "Go back and do more assignments."
+        }
+    }
+}
+
+class PM extends Instructors{
+    constructor(helpful){
+        super(helpful);
+        this.resource = helpful.resource
+    }
+    helping(){
+        console.log(`I found ${this.resource} extremely helpful if you ever get stuck.`)
+    }
+}
